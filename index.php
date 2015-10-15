3f323abd7eceaa89b1f283829041b12539ed4aee
Sergey Podgornyy <sergey.podgornyy@yahoo.de>
2015-10-16 01:31:55 +0300

    Basics structure for the website

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+$name = "Mike";
+$location = "Orlando, FL";
+$full_name = "Mike The Frog";
+$name = $full_name;
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta charset=utf-8>
+  	<title><?php echo $name ?> | Treehouse Profile</title>
+  	<link href="css/style.css" rel="stylesheet" />
+  </head>
+  
+  <body>
+    <section class="sidebar text-center">
+      <div class="avatar">
+        <img src="img/avatar.png" alt="Mike The Frog">
+      </div>
+      <h1><?php echo $name ?></h1>
+      <p><?php echo $location?></p>
+      <hr />
+      <p>Welcome to PHP Basics!</p>
+      <hr />
+      <ul class="social">
+        <li><a href=""><span class="icon twitter"></span></a></li>
+      </ul>
+    </section>
+    <section class="main">
+      <p>Let's Get Started!</p>
+      <p><?php echo "Hello World";?></p>
+    </section>
+  </body>
+</html>
