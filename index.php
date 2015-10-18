721d954030ab84d41b8bad2543b74f7d5f6d157d
Sergey Podgornyy <sergey.podgornyy@yahoo.de>
2015-10-18 23:00:48 +0300

    Applying Functions

--- index.php
+++ index.php
@@ -10,6 +10,29 @@ $location = "Kiev, Ukraine";
 $role="Student";
 //$full_name = "Mike The Frog";
 
+function hello($names, $time_of_day){
+  if(is_array($names)){
+    foreach ($names as $name) {
+      echo "Hello, $name! $time_of_day How's it going? <br>";
+    }
+  }
+  else{
+    echo "Hello, friend, good $time_of_day! How's it going? <br>";
+  }
+}
+
+function greet($friend, $time_of_day=NULL){
+  if($time_of_day){
+    $time = 'Good '.$time_of_day.',';
+  }
+  else{
+    $time = '';
+  }
+  hello($friend, $time);
+}
+
+$friends=array('Hampton','Mike','Charley','Sergey');
+
 if(USE_FULL_NAME)
   $full_name = $name.' '.$lastname;
 else
@@ -58,8 +81,9 @@ $social_icons = array('twitter'=>'https://twitter.com/KoeplerF', 'google'=>'http
     </section>
     <section class="main">
       <p><?php echo $info;?></p>
+      <br>
+      <p><?php greet($friends, 'morning')?></p><br>
       <p>Let's Get Started!</p>
-      <p><?php echo "Hello World";?></p>
     </section>
   </body>
 </html>
